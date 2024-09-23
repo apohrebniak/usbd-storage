27350b4ff0b349f58bd97a4a4a9c7af2b35c5ed9
Alex Kazik <alex@kazik.de>
2024-09-23 22:58:12 +0200

    support lba/len being u32

--- usbd-storage/src/subclass/scsi.rs
+++ usbd-storage/src/subclass/scsi.rs
@@ -32,8 +32,10 @@ const MODE_SENSE_10: u8 = 0x5A;
 
 /* SBC */
 const READ_10: u8 = 0x28;
+#[cfg(feature = "u64")]
 const READ_16: u8 = 0x88;
 const READ_CAPACITY_10: u8 = 0x25;
+#[cfg(feature = "u64")]
 const READ_CAPACITY_16: u8 = 0x9E;
 const WRITE_10: u8 = 0x2A;
 
@@ -77,16 +79,29 @@ pub enum ScsiCommand {
 
     /* SBC */
     ReadCapacity10,
+    #[cfg(feature = "u64")]
     ReadCapacity16 {
         alloc_len: u32,
     },
     Read {
+        #[cfg(feature = "u64")]
         lba: u64,
+        #[cfg(not(feature = "u64"))]
+        lba: u32,
+        #[cfg(feature = "u64")]
         len: u64,
+        #[cfg(not(feature = "u64"))]
+        len: u32,
     },
     Write {
+        #[cfg(feature = "u64")]
         lba: u64,
+        #[cfg(not(feature = "u64"))]
+        lba: u32,
+        #[cfg(feature = "u64")]
         len: u64,
+        #[cfg(not(feature = "u64"))]
+        len: u32,
     },
 
     /* MMC */
@@ -119,20 +134,24 @@ fn parse_cb(cb: &[u8]) -> ScsiCommand {
             alloc_len: cb[4],
         },
         READ_CAPACITY_10 => ScsiCommand::ReadCapacity10,
+        #[cfg(feature = "u64")]
         READ_CAPACITY_16 => ScsiCommand::ReadCapacity16 {
             alloc_len: u32::from_be_bytes([cb[10], cb[11], cb[12], cb[13]]),
         },
         READ_10 => ScsiCommand::Read {
-            lba: u32::from_be_bytes([cb[2], cb[3], cb[4], cb[5]]) as u64,
-            len: u16::from_be_bytes([cb[7], cb[8]]) as u64,
+            #[cfg_attr(not(feature = "u64"), allow(clippy::useless_conversion))]
+            lba: u32::from_be_bytes([cb[2], cb[3], cb[4], cb[5]]).into(),
+            len: u16::from_be_bytes([cb[7], cb[8]]).into(),
         },
+        #[cfg(feature = "u64")]
         READ_16 => ScsiCommand::Read {
             lba: u64::from_be_bytes((&cb[2..10]).try_into().unwrap()),
-            len: u32::from_be_bytes((&cb[10..14]).try_into().unwrap()) as u64,
+            len: u32::from_be_bytes((&cb[10..14]).try_into().unwrap()).into(),
         },
         WRITE_10 => ScsiCommand::Write {
-            lba: u32::from_be_bytes([cb[2], cb[3], cb[4], cb[5]]) as u64,
-            len: u16::from_be_bytes([cb[7], cb[8]]) as u64,
+            #[cfg_attr(not(feature = "u64"), allow(clippy::useless_conversion))]
+            lba: u32::from_be_bytes([cb[2], cb[3], cb[4], cb[5]]).into(),
+            len: u16::from_be_bytes([cb[7], cb[8]]).into(),
         },
         MODE_SENSE_6 => ScsiCommand::ModeSense6 {
             dbd: (cb[1] & 0b00001000) != 0,
